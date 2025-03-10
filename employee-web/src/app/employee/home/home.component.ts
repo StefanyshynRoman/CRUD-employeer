4b0129ac6856fd59b003d042a88f129c7d151fea
roman <romanpz051@gmail.com>
2025-03-10 10:46:20 +0100

    0.0.2.1 -front-http

--- employee-web/src/app/employee/home/home.component.ts
+++ employee-web/src/app/employee/home/home.component.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, OnInit} from '@angular/core';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MatTableDataSource, MatTableModule} from '@angular/material/table';
+import {Employee} from '../employee';
+import {EmployeeService} from '../employee.service';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatTableModule
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  displayedColumns = ['id', 'name', 'email', 'salary'];
+  // displayedColumns = ['id', 'name', 'email', 'salary','edit','delete'];
+  dataSource = new MatTableDataSource<Employee>();
+  constructor(private  employeeService:EmployeeService) {
+  }
+  employees: Employee [] = []
 
+  ngAfterViewInit(): void {
+  }
+
+  ngOnInit(): void {
+    this.employeeService.fetchAllEmployees().subscribe((data)=>
+    {
+      this.employees=data;
+      this.dataSource = new MatTableDataSource<Employee>(data);
+    })
+  }
 }
+
